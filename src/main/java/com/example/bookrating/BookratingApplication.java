83bc2456acf10502fae4ac31d8c506e36c0d8163
yyujjin <aba1740@naver.com>
2024-08-21 22:40:36 +0900

    :tada: 프로젝트 생성!

---
+++ src/main/java/com/example/bookrating/BookratingApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookrating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookratingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookratingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookrating/BookratingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookrating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookratingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

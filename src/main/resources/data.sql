879bf74e17cc4d98ba7314faae9026aca755d12a
yyujjin <aba1740@naver.com>
2024-08-21 23:50:22 +0900

    :sparkles: h2 db 생성 후 더미 데이터 넣기

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO tag (name) VALUES
+('Classic'),
+('Dystopian'),
+('Romance'),
+('Historical'),
+('Fantasy'),
+('Literary Fiction'),
+('American'),
+('British'),
+('Young Adult'),
+('Drama');

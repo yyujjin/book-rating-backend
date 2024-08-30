5860724c96a27f89cee9d7a7da3f387d172bdbb0
yyujjin <aba1740@naver.com>
2024-08-30 17:39:23 +0900

    수정,삭제 테스트코드 작성중

--- src/test/java/com/example/bookrating/controller/BookControllerTest.java
+++ src/test/java/com/example/bookrating/controller/BookControllerTest.java
@@ -9,13 +9,27 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
 import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.mockito.Mockito.when;
+import static org.springframework.http.RequestEntity.delete;
 import static org.springframework.test.util.AssertionErrors.assertFalse;
 import static org.springframework.test.util.AssertionErrors.assertTrue;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
+@SpringBootTest
+@AutoConfigureMockMvc
 public class BookControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Mock
     private BookRepository bookRepository;
@@ -66,5 +80,47 @@ public class BookControllerTest {
         //then
         assertTrue("책 저장 실패",true);
     }
+
+    @Test
+    public void 책_수정_성공() {
+        //given
+        Long id = 1L;
+        Book book = new Book(id, "1111", "짱구야놀자");
+
+        // Mock 설정: ID로 책을 조회할 때 book이 반환되도록 설정
+        when(bookRepository.findById(id)).thenReturn(Optional.of(book));
+
+        // Mock 설정: bookRepository.save(book) 호출 시, book 객체가 반환되도록 설정
+        when(bookRepository.save(book)).thenReturn(book);
+
+        //when
+        // 책의 제목을 "세일러문"으로 수정하고, 수정된 book 객체를 반환받음
+        Optional<Book> result = bookService.patchBook(book, "세일러문");
+
+        //then
+        // 반환된 객체의 제목이 "세일러문"으로 변경되었는지 확인
+        assertEquals("세일러문", result.get().getTitle());
+    }
+
+    @Test
+    public void 책_삭제(){
+        //given
+        Book book = new Book(1L,"1111","스폰지밥");
+        Long id = 1L;
+        when(bookRepository.findById(id)).thenReturn(Optional.of(book));
+        bookRepository.deleteById(id);
+
+        //when
+        bookService.deleteBook(id);
+
+        //when & then
+        mockMvc.perform(delete("/books/{id}", id))
+                .andExpect(status().isNoContent());  // 204 상태 코드 확인
+
+
+
+    }
+
+
 
 }

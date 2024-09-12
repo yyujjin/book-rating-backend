8a45f1bfe047532ff1eebeef762ceb0f4dec9214
yyujjin <141253669+yyujjin@users.noreply.github.com>
2024-09-12 22:03:00 +0900

    :recycle: if-else 제거를 통한 로직 가독성 개선

--- src/main/java/com/example/bookrating/controller/ReviewController.java
+++ src/main/java/com/example/bookrating/controller/ReviewController.java
@@ -27,25 +27,25 @@ public class ReviewController {
     @PostMapping("/books/{bookId}/reviews")
     public ResponseEntity<?>postReview(@PathVariable Long bookId, @ModelAttribute Review review){
         boolean isReviewPosted = reviewService.postReview(bookId,review);
-       if (isReviewPosted){
-           return ResponseEntity.ok().body("리뷰가 등록되었습니다.");
+       if (!isReviewPosted){
+           return ResponseEntity.badRequest().body("리뷰 등록에 실패하였습니다.");
        }
-        return ResponseEntity.badRequest().body("리뷰 등록에 실패하였습니다.");
+        return ResponseEntity.ok().body("리뷰가 등록되었습니다.");
     }
 
     @PatchMapping("books/{bookId}/reviews/{reviewId}")
     public ResponseEntity<?>patchReview(@PathVariable Long bookId,@PathVariable Long reviewId,@RequestBody String content){
+
         //리뷰 찾기
-        if (reviewService.findById(reviewId).isPresent()){
-            //리뷰 수정 로직 작성
-            Optional<Review> updatedReview = reviewService.patchReview(bookId,reviewId,content);
-            return updatedReview
-                    .map(review -> ResponseEntity.ok().body(review.getContent()))
-                    .orElse(ResponseEntity.badRequest().body("리뷰 변경에 실패하였습니다."));
-        }else {
-            //존재하지 않는 리뷰
+        if (reviewService.findById(reviewId).isEmpty()){
             return ResponseEntity.badRequest().body("존재하지 않는 리뷰입니다.");
         }
+
+        Optional<Review> updatedReview = reviewService.patchReview(bookId,reviewId,content);
+        return updatedReview
+                .map(review -> ResponseEntity.ok().body(review.getContent()))
+                .orElse(ResponseEntity.badRequest().body("리뷰 변경에 실패하였습니다."));
+
     }
 
     @DeleteMapping("/books/{bookId}/reviews/{reviewId}")

b967195cb756f571cf8017c677d62e9937cc00b8
yyujjin <141253669+yyujjin@users.noreply.github.com>
2024-10-18 21:28:27 +0900

    :sparkles: 로그인 시 OAuth2 사용자 정보 처리 로직 커스터마이징 추가

--- src/main/java/com/example/bookrating/config/SecurityConfig.java
+++ src/main/java/com/example/bookrating/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.bookrating.config;
 
+import com.example.bookrating.service.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -11,6 +12,15 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
+        this.customOAuth2UserService = customOAuth2UserService;
+    }
+
+
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
@@ -28,7 +38,10 @@ public class SecurityConfig {
 
         //oauth2
         http
-                .oauth2Login(Customizer.withDefaults());
+                .oauth2Login((oauth2)->oauth2
+                        .userInfoEndpoint((userInfoEndpointConfig ->userInfoEndpointConfig
+                                .userService(customOAuth2UserService))));
+
 
         //경로별 인가 작업
         http

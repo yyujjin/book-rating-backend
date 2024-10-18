c2d318042dfafdf3a71a79c9c1a71fdae260d708
yyujjin <141253669+yyujjin@users.noreply.github.com>
2024-10-18 21:54:00 +0900

    :sparkles: UserDTO 구글아이디, 권한 변수 추가

--- src/main/java/com/example/bookrating/dto/UserDTO.java
+++ src/main/java/com/example/bookrating/dto/UserDTO.java
@@ -5,7 +5,9 @@ import lombok.Data;
 @Data
 public class UserDTO {
     private Long id;
-    private String email;
-    private String userName;
-    private String avatar;
+    private String userName; //google + 제공자에서 발급해주는 아이디(번호)
+    private String email; //이메일
+    private String name; //유저 이름
+    private String role; //권한
+    private String avatar; //토큰
 }

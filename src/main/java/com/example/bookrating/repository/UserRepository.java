c4723310b35ac7d57147ffcf782d31c6446cf55e
yyujjin <141253669+yyujjin@users.noreply.github.com>
2024-10-18 22:21:32 +0900

    :sparkles: Username으로 기존 유저 확인 후 없으면 저장, 있으면 업데이트 로직 추가

---
+++ src/main/java/com/example/bookrating/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.bookrating.repository;
+
+import com.example.bookrating.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity,Long> {
+
+    UserEntity findByUsername(String username);
+
+}
--- src/main/java/com/example/bookrating/service/CustomOAuth2UserService.java
+++ src/main/java/com/example/bookrating/service/CustomOAuth2UserService.java
@@ -3,6 +3,8 @@ package com.example.bookrating.service;
 import com.example.bookrating.dto.CustomOAuth2User;
 import com.example.bookrating.dto.GoogleResponseDTO;
 import com.example.bookrating.dto.UserDTO;
+import com.example.bookrating.entity.UserEntity;
+import com.example.bookrating.repository.UserRepository;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -11,6 +13,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final UserRepository userRepository;
+
+    public CustomOAuth2UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException{
@@ -21,11 +29,37 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         if (registrationId.equals("google")){
             GoogleResponseDTO googleResponseDTO = new GoogleResponseDTO(oAuth2User.getAttributes());
             String username = googleResponseDTO.getProvider()+" "+googleResponseDTO.getProviderId();
-            UserDTO userDTO = new UserDTO();
-            userDTO.setUserName(username);
-            userDTO.setName(googleResponseDTO.getName());
-            userDTO.setRole("ROLE_USER");
-            return new CustomOAuth2User(userDTO);
+
+            UserEntity existData = userRepository.findByUsername(username);
+
+            if (existData==null){
+                UserEntity userEntity = new UserEntity();
+                userEntity.setUsername(username);
+                userEntity.setEmail(googleResponseDTO.getEmail());
+                userEntity.setName(googleResponseDTO.getName());
+                userEntity.setRole("ROLE_USER");
+
+                userRepository.save(userEntity);
+
+                UserDTO userDTO = new UserDTO();
+                userDTO.setUserName(username);
+                userDTO.setName(googleResponseDTO.getName());
+                userDTO.setRole("ROLE_USER");
+                return new CustomOAuth2User(userDTO);
+
+            }else {
+                existData.setEmail(googleResponseDTO.getEmail());
+                existData.setName(googleResponseDTO.getName());
+                userRepository.save(existData);
+
+                UserDTO userDTO = new UserDTO();
+                userDTO.setUserName(existData.getUsername());
+                userDTO.setName(googleResponseDTO.getName());
+                userDTO.setRole(existData.getRole());
+
+                return new CustomOAuth2User(userDTO);
+            }
+
         }
         return null;
     }

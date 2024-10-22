2295631dfa023832898ce27b8f965062c00c4dc8
yyujjin <141253669+yyujjin@users.noreply.github.com>
2024-10-22 19:59:00 +0900

    🐛 @Component 추가

--- src/main/java/com/example/bookrating/controller/CustomLogoutHandler.java
+++ src/main/java/com/example/bookrating/controller/CustomLogoutHandler.java
@@ -5,7 +5,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CustomLogoutHandler implements LogoutHandler {
     @Override
     public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {

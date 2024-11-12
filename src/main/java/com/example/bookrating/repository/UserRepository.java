dd56d18fa6c0255e60f6aaf30161424c4c023790
yyujjin <141253669+yyujjin@users.noreply.github.com>
2024-11-12 22:21:30 +0900

    ♻️ UserRepository 메서드 수정 - findByUsername에서 findByProviderId로 변경

--- src/main/java/com/example/bookrating/repository/UserRepository.java
+++ src/main/java/com/example/bookrating/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.example.bookrating.repository;
 import com.example.bookrating.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
 
-    UserEntity findByUsername(String username);
+    UserEntity findByProviderId(String providerId);
 }

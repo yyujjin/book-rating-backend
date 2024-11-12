9ac376b39ef4be757536082242a6e433b0bab430
yyujjin <141253669+yyujjin@users.noreply.github.com>
2024-11-12 20:42:03 +0900

    🔧 토큰에서 사용자 정보 추출 로직 분리 및 TokenExtractor 클래스 생성

--- src/main/java/com/example/bookrating/controller/UserController.java
+++ src/main/java/com/example/bookrating/controller/UserController.java
@@ -1,62 +1,32 @@
 package com.example.bookrating.controller;
 
 import com.example.bookrating.dto.UserDTO;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.Jwts;
-import jakarta.servlet.http.Cookie;
+import com.example.bookrating.util.TokenExtractor;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-
-
 @RestController
 @CrossOrigin
 public class UserController {
-    @Value("${jwt.secret}")
-    private String SECRET_KEY;
+
+    private final TokenExtractor tokenExtractor;
+
+    public UserController(TokenExtractor tokenExtractor) {
+        this.tokenExtractor = tokenExtractor;
+    }
 
     @PostMapping("/loginInfo")
     public ResponseEntity<UserDTO> login(HttpServletRequest request) {
-        String token = getJwtFromCookies(request);
 
-        if (token == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-        try {
-            Claims claims = Jwts.parser()
-                    .setSigningKey(SECRET_KEY.getBytes())
-                    .build()
-                    .parseClaimsJws(token)
-                    .getBody();
+        UserDTO userDTO =  tokenExtractor.getUserInfoFromToken(request);
+        if(userDTO==null) return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
 
-            String username = claims.get("username", String.class);
-            String avatar = claims.get("avatar", String.class);
+        return ResponseEntity.ok(userDTO);
 
-            UserDTO userDTO = new UserDTO();
-            userDTO.setUserName(username);
-            userDTO.setAvatar(avatar);
-
-            return ResponseEntity.ok(userDTO);
-        } catch (JwtException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
     }
 
-    private String getJwtFromCookies(HttpServletRequest request) {
-        if (request.getCookies() != null) {
-            return Arrays.stream(request.getCookies())
-                    .filter(cookie -> "jwt".equals(cookie.getName()))
-                    .map(Cookie::getValue)
-                    .findFirst()
-                    .orElse(null);
-        }
-       return null;
-    }
 }
---
+++ src/main/java/com/example/bookrating/util/TokenExtractor.java
@@ -0,0 +1,47 @@
+package com.example.bookrating.util;
+
+import com.example.bookrating.dto.UserDTO;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class TokenExtractor {
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
+
+    public UserDTO getUserInfoFromToken(HttpServletRequest request) {
+
+        String token = getJwtFromCookies(request);
+
+            Claims claims = Jwts.parser()
+                    .setSigningKey(SECRET_KEY.getBytes())
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            UserDTO userDTO = new UserDTO();
+            userDTO.setProviderId(claims.getSubject());
+            userDTO.setEmail(claims.get("email", String.class));
+            userDTO.setAvatar(claims.get("avatar", String.class));
+            System.out.println("사용자 고유 아이디 :" + claims.getSubject() + "\n 사용자 이메일 : " + userDTO.getEmail() + "\n 사용자 이미지 : " + userDTO.getAvatar());
+
+            return userDTO;
+    }
+
+    private String getJwtFromCookies (HttpServletRequest request){
+        if (request.getCookies() != null) {
+            return Arrays.stream(request.getCookies())
+                    .filter(cookie -> "jwt".equals(cookie.getName()))
+                    .map(Cookie::getValue)
+                    .findFirst()
+                    .orElse(null);
+        }
+        return null;
+    }
+}

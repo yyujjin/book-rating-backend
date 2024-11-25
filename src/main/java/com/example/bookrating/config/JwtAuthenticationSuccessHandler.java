f0f4fe556d464ed1770d26a480934c9660ffdee9
yyujjin <141253669+yyujjin@users.noreply.github.com>
2024-11-25 23:58:52 +0900

    ✨ 로그인 성공 시 사용자 정보를 포함한 JSON 응답 반환 기능 구현

--- src/main/java/com/example/bookrating/config/JwtAuthenticationSuccessHandler.java
+++ src/main/java/com/example/bookrating/config/JwtAuthenticationSuccessHandler.java
@@ -1,6 +1,10 @@
 package com.example.bookrating.config;
 
 import com.example.bookrating.dto.CustomOAuth2UserDTO;
+import com.example.bookrating.dto.UserDTO;
+import com.example.bookrating.entity.UserEntity;
+import com.example.bookrating.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.IOException;
@@ -8,6 +12,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
@@ -15,9 +20,12 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
+@RequiredArgsConstructor
 @Slf4j
 @Component
 public class JwtAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
@@ -27,6 +35,9 @@ public class JwtAuthenticationSuccessHandler implements AuthenticationSuccessHan
 
     @Value("${jwt.secret}")
     private String jwtSecret;
+    private UserDTO userDTO;
+
+    private final UserRepository userRepository;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException, java.io.IOException {
@@ -36,7 +47,11 @@ public class JwtAuthenticationSuccessHandler implements AuthenticationSuccessHan
         if (authentication.getPrincipal() instanceof CustomOAuth2UserDTO) {
             // OAuth2 로그인 사용자
             CustomOAuth2UserDTO customOAuth2UserDTO = (CustomOAuth2UserDTO) authentication.getPrincipal();
-            System.out.println("OAuth2 사용자 정보: " + customOAuth2UserDTO.getAttributes());
+            System.out.println("OAuth2 사용자 로그인 : " + customOAuth2UserDTO.getUsername());
+            UserEntity user = userRepository.findByProviderId(customOAuth2UserDTO.getProviderId());
+            userDTO = new UserDTO();
+            userDTO.setId(userDTO.getId());
+            userDTO.setUsername(userDTO.getUsername());
             token = Jwts.builder()
                     .setSubject(customOAuth2UserDTO.getProviderId()) // 구글 고유 아이디
                     //.claim("authorities", authentication.getAuthorities()) // 권한 정보
@@ -51,9 +66,13 @@ public class JwtAuthenticationSuccessHandler implements AuthenticationSuccessHan
         } else if (authentication.getPrincipal() instanceof UserDetails) {
             // 일반 로그인 사용자
             UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-            System.out.println("일반 사용자 정보: " + userDetails.getUsername());
+            System.out.println("일반 사용자 로그인 : " + userDetails.getUsername());
+            Optional<UserEntity> user = userRepository.findByUsername(userDetails.getUsername());
+            userDTO = new UserDTO();
+            userDTO.setId(user.get().getId());
+            userDTO.setUsername(user.get().getUsername());
             token = Jwts.builder()
-                    //.setSubject(userDetails.getUsername()) // 유저아이디
+                    //.setSubject() // 유저아이디
                     .claim("username",userDetails.getUsername())
                     .setIssuedAt(new Date()) //JWT 토큰이 발급된 시간
                     .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 1일 유효
@@ -64,7 +83,6 @@ public class JwtAuthenticationSuccessHandler implements AuthenticationSuccessHan
             System.out.println("인증되지 않은 사용자");
         }
 
-
         // JWT를 HttpOnly 쿠키에 저장
         Cookie jwtCookie = new Cookie("jwt", token);
         jwtCookie.setHttpOnly(true);  // 자바스크립트 접근 금지
@@ -72,8 +90,18 @@ public class JwtAuthenticationSuccessHandler implements AuthenticationSuccessHan
         jwtCookie.setPath("/");
         jwtCookie.setMaxAge(24 * 60 * 60);  // 24시간 유효
 
-        log.info("로그인에 성공했습니다. 토큰을 발급합니다. Token: " + token);
+        log.info("로그인에 성공했습니다. 토큰을 발급합니다. Token : " + token);
 
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        Map<String, Object> responseBody = new HashMap<>();
+        responseBody.put("user", userDTO);
+
+        String jsonResponse = objectMapper.writeValueAsString(responseBody);
+
+        response.setContentType("application/json; charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(jsonResponse);
         response.addCookie(jwtCookie);
         response.setStatus(201);
     }

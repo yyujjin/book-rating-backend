1e6ad49e685e9f82552529f5441ee60ca42f1855
yyujjin <141253669+yyujjin@users.noreply.github.com>
2024-11-29 21:48:18 +0900

    🧪 springboottest 어노테이션 삭제 후 실행 테스트

--- src/test/java/com/example/bookrating/BookratingApplicationTests.java
+++ src/test/java/com/example/bookrating/BookratingApplicationTests.java
@@ -3,7 +3,6 @@ package com.example.bookrating;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BookratingApplicationTests {
 
 	@Test
